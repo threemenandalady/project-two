582ce5af1e4667ef413a3afb28e0ad9230ec00ab
Brian Holbrook <brian.holbrook56@gmail.com>
2022-03-19 10:14:57 -0500

    yay

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,10 +1,10 @@
 const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+const { User, Posts, Categories, Replies } = require('../models');
 
 const userData = require('./userData.json');
 const categories = require('./categories.json');
 const replies = require('./replies.json');
-const messageData = require('./messagedata.json')
+const posts = require('./posts.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -14,9 +14,22 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
+  for (const category of categories) {
+    await Categories.create({
+      ...category,
+    });
+  }
+
+  for (const post of posts) {
+    await Posts.create({
+      ...post,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  for (const reply of replies) {
+    await Replies.create({
+      ...reply,
       user_id: users[Math.floor(Math.random() * users.length)].id,
     });
   }
