4ad05ae720867d765bd389dc251c54fb2dc36268
bgould86 <bgould86@gmail.com>
2022-03-22 17:49:48 -0500

    filtering results

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,49 @@
 const router = require('express').Router();
 const { Posts, User, Categories, Replies } = require('../models');
+const { sequelize } = require('../models/User');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
     // Get all projects and JOIN with user data
     const postData = await Posts.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+        {
+          model: Categories,
+          attributes: ['name'],
+        },
+        {
+          model: Replies,
+          attributes: ['message'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const posts = postData.map((post) => post.get({ plain: true }));
+    console.log(posts);
+    // Pass serialized data and session flag into template
+    res.render('homepage', {
+      posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// filters posts based on category
+router.get('/posts/:categories_id', async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const postData = await Posts.findAll({
+      where: {
+        categories_id: req.params.categories_id,
+      },
       include: [
         {
           model: User,
